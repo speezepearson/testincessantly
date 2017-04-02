5e70a351fd73cd4145cf4c9ea9ba0efbf2c53bbb
Spencer Pearson <suspense@cs.washington.edu>
2017-04-01 19:12:03 -0700

    change exclusion rules
    files are now excluded if any segment of the path matches any given pattern

--- testincessantly/exclusion.py
+++ testincessantly/exclusion.py
@@ -1,10 +1,22 @@
+import os.path
 import fnmatch
 
 def parse_pattern_file(file):
-  stripped_lines = (line.strip() for line in file)
+  stripped_lines = (line.strip().strip('/') for line in file)
   return set(line for line in stripped_lines if line and not line.startswith('#'))
 
+def iter_segments(path):
+  path = os.path.normpath(path)
+  while path not in ['', '/']:
+    path, basename = os.path.split(path)
+    if basename: yield basename
+
 def exclude_paths_matching_patterns(paths, patterns):
-  return set(
-    path for path in paths
-    if not any(fnmatch.fnmatch(path, pattern) for pattern in patterns))
+  paths = set(paths)
+  excluded_paths = set(
+    path
+    for path in paths
+    for segment in iter_segments(path)
+    for pattern in patterns
+    if fnmatch.fnmatch(segment, pattern))
+  return paths - excluded_paths
