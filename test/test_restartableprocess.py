8f233416e749b10f53d151990456b782c89b3b88
Spencer Pearson <suspense@cs.washington.edu>
2017-04-03 00:02:42 -0700

    fix timeout out waiting for RestartableProcesses to be killed

--- test/test_restartableprocess.py
+++ test/test_restartableprocess.py
@@ -16,6 +16,18 @@ def test_runs_when_restarted():
   p.process.wait()
   assert p.process.stdout.readline() == b'hi\n'
 
+def test_stop():
+  p = testincessantly.RestartableProcess(['sleep', 'infinity'])
+  p.start()
+  p.stop()
+
+def test_stop_respects_timeout():
+  p = testincessantly.RestartableProcess('trap "true" TERM; echo "I am invincible!"; sleep infinity', shell=True, stdout=subprocess.PIPE)
+  p.start()
+  p.process.stdout.readline()
+  p.stop(timeout=0.01)
+  assert p.process.returncode == -9
+
 def test_context_management():
   with testincessantly.RestartableProcess(['echo', 'hi'], stdout=subprocess.PIPE) as p:
     p.process.wait()
--- testincessantly/restartableprocess.py
+++ testincessantly/restartableprocess.py
@@ -10,9 +10,12 @@ class RestartableProcess:
     self.process = subprocess.Popen(*self.popen_args, **self.popen_kwargs)
   def stop(self, timeout=None):
     if self.process is not None:
-      self.process.kill()
-      self.process.wait(timeout)
-      self.process.kill()
+      self.process.terminate() # try to be nice about it, send SIGTERM
+      try:
+        self.process.wait(timeout=timeout)
+      except subprocess.TimeoutExpired:
+        self.process.kill() # process isn't responding to SIGTERM; murder it
+        self.process.wait()
   def restart(self):
     self.stop()
     self.start()
